b3a97b88e5bc006153a0fc76854cc0df940dd090
Tom Meyers <zeus@zeus.ap.be>
2018-12-03 14:04:38 +0100

    Updated placeholder data

--- node/environment.js
+++ node/environment.js
@@ -2,57 +2,174 @@
 
   let Data = {
     timeout: 5000,
-    language1: {
-      title: "Language 1",
-      url: "/get/lan1",
+    java_menu: {
+      title: "java",
+      url: "/language/java",
       children: [{
-          title: "part 1",
-          url: "/get/lan1/part1",
+          title: "java",
+          url: "/language/java",
           children: []
         },
         {
-          title: "part 2",
-          url: "/get/lan1/part2",
+          title: "java advanced",
+          url: "/languages/java_advanced",
           children: []
-        },
-        {
-          title: "part 3",
-          url: "/get/lan1/part3",
-          children: []
-        },
+        }
       ]
     },
-    language2: {
-      title: "Language 2",
-      url: "/get/lan2",
+    javascript_menu: {
+      title: "javascript",
+      url: "/language/javascript",
       children: [{
-          title: "part 1",
-          url: "/get/lan2/part1",
+        title: "javascript",
+        url: "/language/javascript",
+        children: []
+      }]
+    },
+
+    csharp_menu: {
+      title: "CSharp",
+      url: "/languages/csharp",
+      children: [{
+          title: "basics",
+          url: "/languages/csharp",
           children: []
         },
         {
-          title: "part 2",
-          url: "/get/lan2/part2",
+          title: "classes",
+          url: "/languages/csharp_classes",
           children: []
         },
         {
-          title: "part 3",
-          url: "/get/lan3/part3",
+          title: "OOP",
+          url: "/languages/csharp_oop",
           children: [{
-              title: "part 3 section 1",
-              url: "/get/lan2/part3/section1",
+              title: "basics",
+              url: "/languages/csharp_oop_basic",
               children: []
             },
             {
-              title: "part 3 section 2",
-              url: "/get/lan2/part3/section2",
+              title: "polymorphism",
+              url: "/languages/csharp_polymorphism",
               children: []
             },
           ]
         },
       ]
+    },
+    csharp: {
+      title: "csharp",
+      data: [{
+          title: "main method",
+          subtitle: "the main method",
+          gist: "www.github.com"
+        },
+        {
+          title: "variables",
+          subtitle: "basic variables",
+          gist: "www.github.com"
+        },
+
+      ]
+    },
+    csharp_polymorphism: {
+      title: "csharp_polymorphism",
+      data: [{
+          title: "main method",
+          subtitle: "the main method",
+          gist: "www.github.com"
+        },
+        {
+          title: "variables",
+          subtitle: "basic variables",
+          gist: "www.github.com"
+        },
+
+      ]
+    },
+    csharp_oop_basic: {
+      title: "csharp_oop_basic",
+      data: [{
+          title: "main method",
+          subtitle: "the main method",
+          gist: "www.github.com"
+        },
+        {
+          title: "variables",
+          subtitle: "basic variables",
+          gist: "www.github.com"
+        },
+
+      ]
+    },
+    csharp_classes: {
+      title: "csharp_classes",
+      data: [{
+          title: "main method",
+          subtitle: "the main method",
+          gist: "www.github.com"
+        },
+        {
+          title: "variables",
+          subtitle: "basic variables",
+          gist: "www.github.com"
+        },
+
+      ]
+    },
+
+    java: {
+      title: "java",
+      data: [{
+          title: "java basics",
+          subtitle: "What is java",
+          gist: "www.github.com"
+        },
+        {
+          title: "java basics 2.0",
+          subtitle: "how to read java",
+          gist: "www.github.com"
+        },
+
+      ]
+    },
+    java_advanced: {
+      title: "java_advanced",
+      data: [{
+          title: "OOP",
+          subtitle: "Opject oriented code",
+          gist: "www.github.com"
+        },
+        {
+          title: "OOP extended",
+          subtitle: "Extra info",
+          gist: "www.github.com"
+        },
+
+      ]
+    },
+    javascript: {
+      title: "javascript",
+      data: [{
+          title: "Variables",
+          subtitle: "How to use javascript variables",
+          gist: "www.github.com"
+        },
+        {
+          title: "function",
+          subtitle: "Basic javascript functions",
+          gist: "www.github.com"
+        },
+        {
+          title: "classes",
+          subtitle: "EM6 classes vs prototype"
+        }
+
+      ]
     }
+
   }
+
 
   module.exports.data = Data;
 
