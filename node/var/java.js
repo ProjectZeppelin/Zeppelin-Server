03e091198936301a370499d673beafc2823e45f8
Tom Meyers <zeus@zeus.ap.be>
2019-01-06 18:22:39 +0100

    Updated java cheatsheet with the basics

--- node/var/java.js
+++ node/var/java.js
@@ -12,6 +12,46 @@
           subtitle: "java primitieve variabelen",
           gist: "https://aplearning.xyz/zeppelin/snippets/?F0xedb/3302a198e03d47c7981b4ff4234cea67/raw/38fd609a15cf2e884adb6db3ebff82e0f7d04286/Variables.java"
         },
+        {
+          title: "For Loop",
+          subtitle: "A for loop can be run x amount of times",
+          gist: "https://aplearning.xyz/zeppelin/snippets/?F0xedb/2e51cb5ea8c5c39b34166680d8b2acd1/raw/cb9ab81e504a3c06eb8eb77a17f119188baf497f/For%2520Loop.java"
+        },
+        {
+          title: "While",
+          subtitle: "A loop that keeps running until a condition is true",
+          gist: "https://aplearning.xyz/zeppelin/snippets/?F0xedb/85370094b3f4aa685488bbe03d8078da/raw/7da99c129ac70f6f98a7cff2671ea933189c666f/While.java"
+        },
+        {
+          title: "If",
+          subtitle: "A statement for defining logic",
+          gist: "https://aplearning.xyz/zeppelin/snippets/?F0xedb/8f5ae7432065bc7a9e5dc0ec84d192b9/raw/f57311f45f11d5cb314b943b850f1da969bac164/If.java"
+        },
+        {
+          title: "Tenary operator",
+          subtitle: "This operator is a short hand notation of an if statement",
+          gist: "https://aplearning.xyz/zeppelin/snippets/?F0xedb/c2f3fb50c455667bb44c56f698d0a40f/raw/04a4f137d20d8b307cd4b022483646423916e15e/Tenary%2520operator.java"
+        },
+        {
+          title: "Logic Operators",
+          subtitle: "used for boolean expressions",
+          gist: "https://aplearning.xyz/zeppelin/snippets/?F0xedb/50b7f3e7f400fcd89c22a0d89daada78/raw/f62afe24d3d809019748062ef9b1d23ffdbcc1a6/Logic%2520Operators.java"
+        },
+        {
+          title: "Switch",
+          subtitle: "Short hand notation for an if statement (only ==)",
+          gist: "https://aplearning.xyz/zeppelin/snippets/?F0xedb/53ce0f2bd579dbf4b408457458505190/raw/067d43c93b639544667b1d9c6830ba0b89ce39cc/Switch.java"
+        },
+        {
+          title: "Methods",
+          subtitle: "Blocks of code which you can repeat",
+          gist: "https://aplearning.xyz/zeppelin/snippets/?F0xedb/e326256e0bda4e4d968ab04035041606/raw/a8ec6dbe92ed2d74521617d8c3d6e4db367e35f8/Methods.java"
+        },
+        {
+          title: "Method return types",
+          subtitle: "What we can return from Methods",
+          gist: "https://aplearning.xyz/zeppelin/snippets/?F0xedb/a603dddefee033893b22b7f0960f3790/raw/122eb43575ac01a5d82d7d09be30b7d94b2e7b87/Method%2520return%2520types.java"
+        },
       ]
     }
   }
