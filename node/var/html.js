423a448f272f218848b2dd1a214a0e2b049cf0c9
Tom Meyers <zeus@zeus.ap.be>
2019-01-06 18:40:03 +0100

    Added start of html

--- node/var/html.js
+++ node/var/html.js
@@ -4,18 +4,18 @@
       title: "html",
       data: [{
           title: "Template",
-          subtitle: "Temp",
-          gist: "https://aplearning.xyz/zeppelin/snippets/?F0xedb/"
+          subtitle: "Always use this template",
+          gist: "https://aplearning.xyz/zeppelin/snippets/?F0xedb/bb344fa5bfe4f6764737569ba26eb5f1/raw/8f034890c27f44e6783be2c31cf60c3782fb4f46/Template.html"
         },
         {
-          title: "Template",
-          subtitle: "Temp",
-          gist: "https://aplearning.xyz/zeppelin/snippets/?F0xedb/"
+          title: "External CSS",
+          subtitle: "Link an external css file",
+          gist: "https://aplearning.xyz/zeppelin/snippets/?F0xedb/103e6224f3f30e2f372a736d9062953f/raw/e473483c5ac6f6037cea600f607077c370a9a8d3/External%2520CSS.html"
         },
         {
-          title: "Template",
-          subtitle: "Temp",
-          gist: "https://aplearning.xyz/zeppelin/snippets/?F0xedb/"
+          title: "External Script",
+          subtitle: "Link an external js file",
+          gist: "https://aplearning.xyz/zeppelin/snippets/?F0xedb/dadaf4afc4e7fb8f3b5d87e76feb91d3/raw/84da6d324489eda19b887843181d8ed44e02f778/External%2520Script.html"
         },
       ]
     }
