3278afdd01a17090a49a900c135efc669551d091
root <Tom>
2019-01-06 17:03:37 +0000

    Old javascript

--- node/var/javascript.js
+++ node/var/javascript.js
@@ -5,12 +5,12 @@
       data: [{
           title: "Variables",
           subtitle: "How to use javascript variables",
-          gist: "https://gist.github.com/F0xedb/56c7f68b5de9d7916e03b91dbe37c745.js"
+          gist: "https://aplearning.xyz/zeppelin/snippets/?F0xedb/56c7f68b5de9d7916e03b91dbe37c745/raw/1e120d21cb51d2b5709fb94992b337abc4b4b52f/variables.js"
         },
         {
           title: "function",
           subtitle: "Basic javascript functions",
-          gist: "https://gist.github.com/F0xedb/0d37db8bf9dbc8b017c99c27e9e3bd2f.js"
+          gist: "https://aplearning.xyz/zeppelin/snippets/?F0xedb/0d37db8bf9dbc8b017c99c27e9e3bd2f/raw/cf377fa7e6e61657221dc100508b025e0dbba746/functions.js"
         },
       ]
     }
