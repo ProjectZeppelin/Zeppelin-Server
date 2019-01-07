6ae93769978f6529f376423703e8d18e28a17af6
MattZK <mattwillemsen@icloud.com>
2019-01-07 11:50:46 +0100

    Updated Gist URL

--- node/var/csharp.js
+++ node/var/csharp.js
@@ -5,87 +5,87 @@
       data: [{
           title: "Variables",
           subtitle: "C# primitieve variabelen",
-          gist: "https://aplearning.xyz/zeppelin/snippets/?F0xedb/ff30bb3ab0d95acee40de3ab67894329/raw/7438c395157c660ec128e8b40a8bd5bd510b6f75/Variables.cs"
+          gist: "https://aplearning.xyz/zeppelin/snippets/?snippet=F0xedb/ff30bb3ab0d95acee40de3ab67894329/raw/7438c395157c660ec128e8b40a8bd5bd510b6f75/Variables.cs"
         },
         {
           title: "Enumerator",
           subtitle: "An alias for a set of integers used to make states easier on the programmer",
-          gist: "https://aplearning.xyz/zeppelin/snippets/?F0xedb/f57f0ff6d8be62efcedd507b6428b3d1/raw/a2eed3e97e1637d4fc042c31f5eba43a5ce87479/Enumerator.cs"
+          gist: "https://aplearning.xyz/zeppelin/snippets/?snippet=F0xedb/f57f0ff6d8be62efcedd507b6428b3d1/raw/a2eed3e97e1637d4fc042c31f5eba43a5ce87479/Enumerator.cs"
         },
         {
           title: "For Loop",
           subtitle: "A for loop can be run x amount of times",
-          gist: "https://aplearning.xyz/zeppelin/snippets/?F0xedb/2d7b949519d3e0a6d5f2f055c397f10a/raw/f91900e39ca3b5774f5da03af54c4208a805fb9a/For%2520Loop.cs"
+          gist: "https://aplearning.xyz/zeppelin/snippets/?snippet=F0xedb/2d7b949519d3e0a6d5f2f055c397f10a/raw/f91900e39ca3b5774f5da03af54c4208a805fb9a/For%2520Loop.cs"
         },
         {
           title: "While",
           subtitle: "A loop that keeps running until a condition is true",
-          gist: "https://aplearning.xyz/zeppelin/snippets/?F0xedb/5c98989efe42bf2f843f3cfa44063d9d/raw/988cae6a3ee3b2f20d7a1a1c96cdfebbf1b5b6f4/While.cs"
+          gist: "https://aplearning.xyz/zeppelin/snippets/?snippet=F0xedb/5c98989efe42bf2f843f3cfa44063d9d/raw/988cae6a3ee3b2f20d7a1a1c96cdfebbf1b5b6f4/While.cs"
         },
         {
           title: "If",
           subtitle: "A statement for defining logic",
-          gist: "https://aplearning.xyz/zeppelin/snippets/?F0xedb/dfc0db56c6285338ca678792e01472a3/raw/46b235cb3ee8cbd847b412de22f8f8b5912c45cc/If.cs"
+          gist: "https://aplearning.xyz/zeppelin/snippets/?snippet=F0xedb/dfc0db56c6285338ca678792e01472a3/raw/46b235cb3ee8cbd847b412de22f8f8b5912c45cc/If.cs"
         },
         {
           title: " Ternary operator",
           subtitle: "This operator is a short hand notation of an if statement",
-          gist: "https://aplearning.xyz/zeppelin/snippets/?F0xedb/0bd8b70e3bb4cc4206c6f7fff19a2f61/raw/c55d283732727be3d6b7abd1d5f004aef023ed61/Ternary%2520operator.cs"
+          gist: "https://aplearning.xyz/zeppelin/snippets/?snippet=F0xedb/0bd8b70e3bb4cc4206c6f7fff19a2f61/raw/c55d283732727be3d6b7abd1d5f004aef023ed61/Ternary%2520operator.cs"
         },
         {
           title: "Logic Operators",
           subtitle: "used for boolean expressions",
-          gist: "https://aplearning.xyz/zeppelin/snippets/?F0xedb/f584787bd3d9be4ca81d6b1999d0d867/raw/6a91478bd526610f6774ad5e1bb1f610180dd783/Logic%2520Operators.cs"
+          gist: "https://aplearning.xyz/zeppelin/snippets/?snippet=F0xedb/f584787bd3d9be4ca81d6b1999d0d867/raw/6a91478bd526610f6774ad5e1bb1f610180dd783/Logic%2520Operators.cs"
         },
         {
           title: "Switch.cs",
           subtitle: "Short hand notation for an if statement (only ==)",
-          gist: "https://aplearning.xyz/zeppelin/snippets/?F0xedb/6829c4cf6caf63d0ba1259968ede030e/raw/65acc94f343971d6a692c10891a65dec8aaa4fbf/Switch.cs"
+          gist: "https://aplearning.xyz/zeppelin/snippets/?snippet=F0xedb/6829c4cf6caf63d0ba1259968ede030e/raw/65acc94f343971d6a692c10891a65dec8aaa4fbf/Switch.cs"
         },
         {
           title: "Methods",
           subtitle: "Blocks of code which you can repeat",
-          gist: "https://aplearning.xyz/zeppelin/snippets/?F0xedb/6bbf338993802e1fcf53310bd30f16b4/raw/4b67eb2d52f56eac72607bd5465ff87dc9b6200a/Methods.cs"
+          gist: "https://aplearning.xyz/zeppelin/snippets/?snippet=F0xedb/6bbf338993802e1fcf53310bd30f16b4/raw/4b67eb2d52f56eac72607bd5465ff87dc9b6200a/Methods.cs"
         },
         {
           title: "Method return types",
           subtitle: "What we can return from Methods",
-          gist: "https://aplearning.xyz/zeppelin/snippets/?F0xedb/535a6cff2f81bd8a579a9e844dbe35cc/raw/f1113b07edc49dfbde265d43b189634abd78963a/Method%2520return%2520types.cs"
+          gist: "https://aplearning.xyz/zeppelin/snippets/?snippet=F0xedb/535a6cff2f81bd8a579a9e844dbe35cc/raw/f1113b07edc49dfbde265d43b189634abd78963a/Method%2520return%2520types.cs"
         },
         {
           title: "Arrays",
           subtitle: "How to define multiple objects of the same type in one variable",
-          gist: "https://aplearning.xyz/zeppelin/snippets/?F0xedb/2195ed9aa9cd4ec67d1f93a2b996f0b2/raw/6e6332aaf373d1dfbff29447c622519d3b83d36d/Arrays.cs"
+          gist: "https://aplearning.xyz/zeppelin/snippets/?snippet=F0xedb/2195ed9aa9cd4ec67d1f93a2b996f0b2/raw/6e6332aaf373d1dfbff29447c622519d3b83d36d/Arrays.cs"
         },
         {
           title: "multi-dimensional Arrays",
           subtitle: "Multi-dimensional arrays are arrays that contains multiple arrays as data.",
-          gist: "https://aplearning.xyz/zeppelin/snippets/?F0xedb/b48d6af35648bd5579586785cf491ef3/raw/34b8b3f187a39673f49c6f0fbf921b14ac5118a6/multi-dimensional%2520Arrays.cs"
+          gist: "https://aplearning.xyz/zeppelin/snippets/?snippet=F0xedb/b48d6af35648bd5579586785cf491ef3/raw/34b8b3f187a39673f49c6f0fbf921b14ac5118a6/multi-dimensional%2520Arrays.cs"
         },
         {
           title: "Struct",
           subtitle: "Structures are a way to organize variables that belong together and are created in the stack",
-          gist: "https://aplearning.xyz/zeppelin/snippets/?F0xedb/3060b9cf6db4a06b357c5ad957228469/raw/68dc79220b1ea132050276215439fa24d6493267/Struct.cs"
+          gist: "https://aplearning.xyz/zeppelin/snippets/?snippet=F0xedb/3060b9cf6db4a06b357c5ad957228469/raw/68dc79220b1ea132050276215439fa24d6493267/Struct.cs"
         },
         {
           title: "Classes",
           subtitle: "Whereas structs are mainly used for fast (in memory) and small groups of variables. Classes are used to create more complex objects (instances) that are created on the heap.",
-          gist: "https://aplearning.xyz/zeppelin/snippets/?F0xedb/1ba6e73ac5d918e9966c9bb4f3c231df/raw/eddbb834a63b342fa36b2cb116e235a807a324a1/Classes.cs"
+          gist: "https://aplearning.xyz/zeppelin/snippets/?snippet=F0xedb/1ba6e73ac5d918e9966c9bb4f3c231df/raw/eddbb834a63b342fa36b2cb116e235a807a324a1/Classes.cs"
         },
         {
           title: "Inheritance",
           subtitle: "Inherits the methods and properties of a parent class",
-          gist: "https://aplearning.xyz/zeppelin/snippets/?F0xedb/78fc01c5a13ea291dbba785ba97f098e/raw/6c4bed93d933f9a50155a8e11ad32c3aa1b6b95e/Inheritance.cs"
+          gist: "https://aplearning.xyz/zeppelin/snippets/?snippet=F0xedb/78fc01c5a13ea291dbba785ba97f098e/raw/6c4bed93d933f9a50155a8e11ad32c3aa1b6b95e/Inheritance.cs"
         },
         {
           title: "Interface",
           subtitle: "Make a template for subclasses each subclass must use the template.",
-          gist: "https://aplearning.xyz/zeppelin/snippets/?F0xedb/986be1f5c33cea8e7efcb40417ce6eac/raw/868d1785dd38ebe9f168d289909b04b396e45925/Interface.cs"
+          gist: "https://aplearning.xyz/zeppelin/snippets/?snippet=F0xedb/986be1f5c33cea8e7efcb40417ce6eac/raw/868d1785dd38ebe9f168d289909b04b396e45925/Interface.cs"
         },
         /*{
           title: "Template",
           subtitle: "Temp",
-          gist: "https://aplearning.xyz/zeppelin/snippets/?F0xedb/"
+          gist: "https://aplearning.xyz/zeppelin/snippets/?snippet=F0xedb/"
         },*/
       ]
     }
--- node/var/html.js
+++ node/var/html.js
@@ -5,32 +5,32 @@
       data: [{
           title: "Template",
           subtitle: "Always use this template",
-          gist: "https://aplearning.xyz/zeppelin/snippets/?F0xedb/bb344fa5bfe4f6764737569ba26eb5f1/raw/8f034890c27f44e6783be2c31cf60c3782fb4f46/Template.html"
+          gist: "https://aplearning.xyz/zeppelin/snippets/?snippet=F0xedb/bb344fa5bfe4f6764737569ba26eb5f1/raw/8f034890c27f44e6783be2c31cf60c3782fb4f46/Template.html"
         },
         {
           title: "External CSS",
           subtitle: "Link an external css file",
-          gist: "https://aplearning.xyz/zeppelin/snippets/?F0xedb/103e6224f3f30e2f372a736d9062953f/raw/e473483c5ac6f6037cea600f607077c370a9a8d3/External%2520CSS.html"
+          gist: "https://aplearning.xyz/zeppelin/snippets/?snippet=F0xedb/103e6224f3f30e2f372a736d9062953f/raw/e473483c5ac6f6037cea600f607077c370a9a8d3/External%2520CSS.html"
         },
         {
           title: "External Script",
           subtitle: "Link an external js file",
-          gist: "https://aplearning.xyz/zeppelin/snippets/?F0xedb/dadaf4afc4e7fb8f3b5d87e76feb91d3/raw/84da6d324489eda19b887843181d8ed44e02f778/External%2520Script.html"
+          gist: "https://aplearning.xyz/zeppelin/snippets/?snippet=F0xedb/dadaf4afc4e7fb8f3b5d87e76feb91d3/raw/84da6d324489eda19b887843181d8ed44e02f778/External%2520Script.html"
         },
         {
           title: "Element Class",
           subtitle: "A non-unique class for an element, used to target multiple elements in css",
-          gist: "https://aplearning.xyz/zeppelin/snippets/?F0xedb/d49a50ea195cc4050cb396c2f77adedf/raw/2c6747efbd4bd22c9bbb114255ff8373aab1ea3c/Element%2520Class.html"
+          gist: "https://aplearning.xyz/zeppelin/snippets/?snippet=F0xedb/d49a50ea195cc4050cb396c2f77adedf/raw/2c6747efbd4bd22c9bbb114255ff8373aab1ea3c/Element%2520Class.html"
         },
         {
           title: "Element Id",
           subtitle: "A unique id for an element",
-          gist: "https://aplearning.xyz/zeppelin/snippets/?F0xedb/a58298b101843ac2d43cf79d0a34b96c/raw/5009c1051f8f8bc9e4d192932b56943c0b7a64dc/Element%2520Id.html"
+          gist: "https://aplearning.xyz/zeppelin/snippets/?snippet=F0xedb/a58298b101843ac2d43cf79d0a34b96c/raw/5009c1051f8f8bc9e4d192932b56943c0b7a64dc/Element%2520Id.html"
         },
         {
           title: "Template",
           subtitle: "Temp",
-          gist: "https://aplearning.xyz/zeppelin/snippets/?F0xedb/"
+          gist: "https://aplearning.xyz/zeppelin/snippets/?snippet=F0xedb/"
         },
       ]
     }
